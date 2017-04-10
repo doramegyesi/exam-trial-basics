2764f21d713d558e0cd350d15efcab7fb69df8ac
Megyesi Dora <dora.megyesi@gmail.com>
2017-04-10 10:36:22 +0200

    prints zeros now so it does not work at all but I cant always get what I want

--- countas/count-as.py
+++ countas/count-as.py
@@ -1,8 +1,23 @@
-def count_as(change_this):
-    pass
+#def count_as(change_this):
+#    pass
     # Create a function that takes a filename as string parameter,
     # counts the occurances of the letter "a", and returns it as a number.
     # If the file does not exist, the function should return 0 and not break.
+
+text = open("afile.txt", "r")
+t = text.read()
+text.close()
+
+def count_as(filename):
+    for letter in t:
+        number_of_as = 0
+        if letter == "a" or letter == "A":
+            number_of_as += 1
+    return number_of_as
+    try:
+        filename = open('filename.txt', 'r')
+    except:
+        print(0)
 
 print(count_as("afile.txt")) # should print 28
 print(count_as("not-a-file")) # should print 0
