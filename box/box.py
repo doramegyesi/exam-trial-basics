8e589ca2fbee85012cf25865e339d4733305121f
Megyesi Dora <dora.megyesi@gmail.com>
2017-04-10 09:32:22 +0200

    the class of 'box' is created with its parameters

--- box/box.py
+++ box/box.py
@@ -1,9 +1,16 @@
-class Cuboid(object):
-    pass
+#class Cuboid(object):
+#    pass
     # Create a class that represents a cuboid:
     # It should take its three dimensions as constructor parameters (numbers)
     # It should have a method called `get_surface` that returns the cuboid's surface
     # It should have a method called `get_volume` that returns the cuboid's volume
+
+
+class Cuboid():
+    def __init__(self, length, width, height):
+        self.length = length
+        self.width = width
+        self.heigth = height
 
 box = Cuboid(10, 20, 30)
 print(box.get_surface()) # should print 2200
