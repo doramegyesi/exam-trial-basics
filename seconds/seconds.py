67f76c729d4b3c182e2d93054b59f772ac4b19ad
Megyesi Dora <dora.megyesi@gmail.com>
2017-04-10 10:01:32 +0200

    aaand now it works, I truly hope that it does properly

--- seconds/seconds.py
+++ seconds/seconds.py
@@ -6,10 +6,7 @@
 
 
 def seconds(the_list):
-    every_other_number = the_list[::2]
+    every_other_number = the_list[1::2]
     return every_other_number
-
-
-
-
+    
 print(seconds([1, 2, 3, 4, 5])) # should print [2, 4]
