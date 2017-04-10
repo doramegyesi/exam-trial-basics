9331f8d4cc004df6b47213abddd547d7eeb57413
Megyesi Dora <dora.megyesi@gmail.com>
2017-04-10 09:59:34 +0200

    first attempt at 'seconds', it works but not in a way it should

--- seconds/seconds.py
+++ seconds/seconds.py
@@ -1,7 +1,15 @@
-def seconds(change_this):
-    pass
+#def seconds(change_this):
+#    pass
     # Create a function that takes a list as a parameter,
     # and returns a new list with every second element from the orignal list
     # example: [1, 2, 3, 4, 5] should produce [2, 4]
+
+
+def seconds(the_list):
+    every_other_number = the_list[::2]
+    return every_other_number
+
+
+
 
 print(seconds([1, 2, 3, 4, 5])) # should print [2, 4]
